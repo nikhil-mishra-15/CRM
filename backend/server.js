7bf79af1cab421536a0c4f5cb5fd91e0f940bc11
nikhil-mishra-15 <nm737813@gmail.com>
2025-11-11 13:03:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
-  "https://apnagharcrm-gng9b2cdcug4abd8.centralindia-01.azurewebsites.net",
+  "https://crmapna-edhchbc9fhbrbsd6.centralindia-01.azurewebsites.net",
   "http://localhost:3000",
   "http://localhost:5173"
 ];
